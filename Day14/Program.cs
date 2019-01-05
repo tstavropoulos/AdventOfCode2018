8bd91e6e384aa1cd8e552d5d6b1cb46b3ac599ec
TStavrop <Trevor.Stavropoulos@gmail.com>
2019-01-04 18:12:12 -0800

    Solved Day 14 Puzzle 2

--- Day14/Program.cs
+++ Day14/Program.cs
@@ -11,6 +11,10 @@ namespace Day14
         //What are the 10 recipes after this:
         private const int INPUT = 939601;
         private const int TARGET = INPUT + 10;
+        private static List<int> recipes;
+
+        //Matching the target sequency backwards.
+        private static int[] targetSequence = new int[] { 1, 0, 6, 9, 3, 9 };
 
         static void Main(string[] args)
         {
@@ -26,13 +30,13 @@ namespace Day14
 
             //The starting configuration is (3)[7]
 
-            List<int> recipes = new List<int>(TARGET + 2) { 3, 7 };
+            recipes = new List<int>(TARGET + 2) { 3, 7 };
 
             //Elf indices
             int elfA = 0;
             int elfB = 1;
 
-            while(recipes.Count < TARGET)
+            while (recipes.Count < TARGET)
             {
                 int newVal = recipes[elfA] + recipes[elfB];
                 if (newVal > 9)
@@ -54,8 +58,76 @@ namespace Day14
 
             Console.WriteLine($"10 recipes after {INPUT}: {string.Join("", recipes.GetRange(INPUT, 10).Select(x => x.ToString()))}");
 
+            Console.WriteLine("");
+            Console.WriteLine("Star 2");
+            Console.WriteLine("");
+
+
+            recipes.Clear();
+            recipes.Add(3);
+            recipes.Add(7);
+
+            //Elf indices
+            elfA = 0;
+            elfB = 1;
+
+            while (true)
+            {
+                int newVal = recipes[elfA] + recipes[elfB];
+                if (newVal > 9)
+                {
+                    if (Add(1))
+                    {
+                        break;
+                    }
+
+                    if (Add(newVal % 10))
+                    {
+                        break;
+                    }
+                    
+                }
+                else
+                {
+                    if (Add(newVal))
+                    {
+                        break;
+                    }
+                }
+
+                elfA += recipes[elfA] + 1;
+                elfB += recipes[elfB] + 1;
+
+                elfA = elfA % recipes.Count;
+                elfB = elfB % recipes.Count;
+            }
+
+
+            Console.WriteLine($"Recipe {INPUT} appears after {recipes.Count - targetSequence.Length} others");
+            Console.WriteLine("");
+
             Console.ReadKey();
 
+        }
+
+        private static bool Add(int value)
+        {
+            recipes.Add(value);
+
+            if (recipes.Count >= 6)
+            {
+                for (int i = 0; i < targetSequence.Length; i++)
+                {
+                    if (recipes[recipes.Count - 1 - i] != targetSequence[i])
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+
+            return false;
         }
     }
 }
